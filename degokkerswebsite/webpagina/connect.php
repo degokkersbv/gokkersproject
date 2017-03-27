e40e5e619c227eba65bb9ee070bfdfdadbca124c
Dion Rodie <Dionrode@hotmail.com>
2017-03-27 15:11:00 +0200

    PatchV3

--- degokkerswebsite/webpagina/connect.php
+++ degokkerswebsite/webpagina/connect.php
@@ -11,9 +11,9 @@ $password = "";
 $dbname = "degokker";
 try {
 //Creating connection for mysql
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $db = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
 }
 catch(PDOException $e)
