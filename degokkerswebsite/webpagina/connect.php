f9589dc8c386cdd463b292cf73fdcd7661824968
Dion Rodie <Dionrode@hotmail.com>
2017-03-27 14:07:56 +0200

    Connector change

--- degokkerswebsite/webpagina/connect.php
+++ degokkerswebsite/webpagina/connect.php
@@ -11,11 +11,11 @@ $host = "localhost:";
 $dbdb = "degokker";
 $table="users";
 
-if (!mysql_select_db($dbdb, mysql_connect($host, $user, $dbpass)))
+if (!mysqli_select_db($dbdb, mysqli_connect($host, $user, $dbpass)))
 {
-     echo "Connectie met de database mislukt.";
-     exit();
+    echo "Connectie met de database mislukt.";
+    exit();
 }
-else {$con = mysql_select_db($dbdb, mysql_connect($host, $user, $dbpass));}
-session_start();
+else {$con = mysqli_select_db($dbdb, mysqli_connect($host, $user, $dbpass));}
+?>
 ?>
